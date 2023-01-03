c11eaf823f407c60c5cebc5f4fc9f1935efe2f40
c0d3runn3r <js@novadynamics.com>
2023-01-02 16:01:39 -0800

    Add processor tests

---
+++ test/testProcessor.js
@@ -0,0 +1,16 @@
+const { Anomaly, Processor } = require("../index.js");
+const assert = require("assert");
+
+
+describe("Processor", function() {
+
+    it("with { fingerprint : true }", function() {
+
+        it("stores fingerprints after state change", async function() {
+            
+            
+        });
+
+    });
+
+});
